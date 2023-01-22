ab2a6655a474526a1ab35602f865d9c5f0cd9203
Stephadivia <111725358+Stephadivia@users.noreply.github.com>
2023-01-21 23:26:39 -0600

    Update Lab10_01_hilos.py

--- Paralelo2/Lab10_01_hilos.py
+++ Paralelo2/Lab10_01_hilos.py
@@ -4,7 +4,25 @@ import math
 import time
 
 def calc():
-    for i in range(0,4000000):
+    for i in range(0, 4000000):
         math.sqrt(i)
 
+threads = []
+cpus = os.cpu_count()
 
+print("Nucleos en el CPU: ", cpus)
+
+for i in range(cpus):
+    print("Registrando el hilo %d" % i)
+    threads.append(Thread(target=calc))
+
+start = time.time()
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end = time.time()
+print("Se tardo: ", end-start)
