25df69b8389bdbaadb1ee34a158edfd097c0a8bb
Stephadivia <111725358+Stephadivia@users.noreply.github.com>
2023-01-21 23:20:30 -0600

    Update Lab10_13_mapeo.py

--- Paralelo2/Lab10_13_mapeo.py
+++ Paralelo2/Lab10_13_mapeo.py
@@ -7,4 +7,29 @@ rank = comm.Get_rank()
 
 n = 40
 x = range(n)
-m = int
+m = int(math.ceil(float(len(x)) / size))
+x_chunk = list(x[rank*m: (rank+1)*m])
+r_chunk = list(map(math.sqrt, x_chunk))
+
+#======================================
+# Una sola lista de todos los datos
+#======================================
+r = comm.allreduce(r_chunk)
+
+#======================================
+# Una matriz con todos los datos
+#======================================
+rr = comm.allgather(r_chunk)
+
+#======================================
+# Una matriz con todos los datos
+#======================================
+rrr = comm.gather(r_chunk, root = 1)
+
+if rank == 0:
+    print(r)
+    print(rr)
+    print(rrr)
+
+if rank == 1:
+    print(rr)
